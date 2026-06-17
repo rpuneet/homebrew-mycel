8c84a8f48acf2d7cab0e683f91814f02ad7d5d60
rpuneet <35069409+rpuneet@users.noreply.github.com>
2026-06-17 12:25:19 +0530

    chore(formula): bump to v0.2.9 (cross-platform)

--- Formula/mycel.rb
+++ Formula/mycel.rb
@@ -1,16 +1,16 @@
 class Mycel < Formula
   desc "CLI-first AI agent orchestration system"
   homepage "https://github.com/rpuneet/mycel"
-  version "0.2.8"
+  version "0.2.9"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/rpuneet/mycel/releases/download/v0.2.8/mycel_0.2.8_darwin_arm64.tar.gz"
-      sha256 "2a253f1793699b96f5de8138654efb22580eaa5eef73e6a52419a00bf0313468"
+      url "https://github.com/rpuneet/mycel/releases/download/v0.2.9/mycel_0.2.9_darwin_arm64.tar.gz"
+      sha256 "08f8f07ae3f259007e56d3fce6b1a5776597cadaae482664ec8ca2ff3bfbe875"
     else
-      url "https://github.com/rpuneet/mycel/releases/download/v0.2.8/mycel_0.2.8_darwin_amd64.tar.gz"
-      sha256 "ad55644e13b7824d98738c1a34e7d18df3c82264035bce2806a0e6a1849b8db1"
+      url "https://github.com/rpuneet/mycel/releases/download/v0.2.9/mycel_0.2.9_darwin_amd64.tar.gz"
+      sha256 "d7f39a343f77e5e0bde345836c78d6da8d04a26ee5bed39ba6be330252dafacd"
     end
 
     def install
@@ -20,11 +20,11 @@ class Mycel < Formula
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/rpuneet/mycel/releases/download/v0.2.8/mycel_0.2.8_linux_arm64.tar.gz"
-      sha256 "f33b396ae01567ad50876e4a309f690876c24b549f0da3dd354db171644c1ed8"
+      url "https://github.com/rpuneet/mycel/releases/download/v0.2.9/mycel_0.2.9_linux_arm64.tar.gz"
+      sha256 "222857bb16558a78d2a87d955d052998a2359f3bef77116d602c12fd1691ac6b"
     elsif Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
-      url "https://github.com/rpuneet/mycel/releases/download/v0.2.8/mycel_0.2.8_linux_amd64.tar.gz"
-      sha256 "120c40c87f6645fe76a181d09c4d9ef6b8fdd61a39e69afb873f811a879c3c54"
+      url "https://github.com/rpuneet/mycel/releases/download/v0.2.9/mycel_0.2.9_linux_amd64.tar.gz"
+      sha256 "e6ad795d3bf360af9e9d80d558a0cfe92978beabc70fbec8a09ab2cb3d817bed"
     end
 
     def install
